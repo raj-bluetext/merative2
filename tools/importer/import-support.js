23bc6d4b00dd2da98e2a8d04c3d82de63310e651
Amol Anand <amol.anand@gmail.com>
2023-07-12 13:55:31 -0400

    support pages import

---
+++ tools/importer/import-support.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-undef */
+const createMetadataBlock = (main, document) => {
+  const meta = {};
+  // add the template
+  meta.Template = 'Support';
+
+  // find the <title> element
+  const title = document.querySelector('title');
+  if (title) {
+    meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
+  }
+
+  // find the <meta property="og:description"> element
+  // const desc = document.querySelector('[property="og:description"]');
+  // if (desc) {
+  //   meta.Description = desc.content;
+  // }
+
+  // helper to create the metadata block
+  const block = WebImporter.Blocks.getMetadataBlock(document, meta);
+
+  // append the block to the main element
+  main.append(block);
+
+  // returning the meta object might be usefull to other rules
+  return meta;
+};
+
+export default {
+  transform: ({
+    // eslint-disable-next-line no-unused-vars
+    document,
+    url,
+  }) => {
+    // Remove unnecessary parts of the content
+    WebImporter.DOMUtils.remove(document.body, ['header', 'footer']);
+    const main = document.body;
+    const results = [];
+
+    // Remove other stuff that shows up in the page
+    const skipToContent = main.querySelector('.button--skipToContent');
+    if (skipToContent) skipToContent.remove();
+    main.querySelectorAll('iframe').forEach((el) => el.remove());
+    main.querySelector('div#onetrust-consent-sdk')?.remove();
+    if (main.querySelector('.cmp-pdfbasicinfo__action-container')) main.querySelector('.cmp-pdfbasicinfo__action-container').remove();
+
+    // move title from span to H1
+    const title = main.querySelector('span.cmp-text__heading-h1');
+    if (title) {
+      const h1 = document.createElement('H1');
+      h1.innerHTML += title.innerHTML.trim();
+      main.prepend(h1);
+      // remove elements already added to blocks from main
+      title.remove();
+    }
+
+    createMetadataBlock(main, document);
+
+    // main page import - "element" is provided, i.e. a docx will be created
+    results.push({
+      element: main,
+      path: new URL(url).pathname,
+    });
+
+    return results;
+  },
+};
