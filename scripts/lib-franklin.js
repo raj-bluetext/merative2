27e70bcf91be6d8e776be3869aab8ac5d2ed6a37
Charity Helms <3883795+helms-charity@users.noreply.github.com>
2023-12-04 12:05:10 -0500

    feat(sidekick): upgrade block library to use new plugin (#296)
    * feat: add new sidekick library plugin version
    
    * feat: add json location for block-library
    
    * feat: add plugin names
    
    * feat: add icon path
    
    * feat: move plugin js to new dir
    
    * fix: spelling
    
    * feat: move library utils file
    
    * feat: create plugin dirs
    
    * fix: typo in path
    
    * fix: updated paths to scripts/
    
    * feat: add the css files
    
    * feat: add blocks js
    
    * fix: testing the removal of individual js
    
    * feature: remove rectangle size constraint
    
    * feature: add dark purple as bg option
    
    * feature: create Stat block library item
    
    * fix: stat font color
    
    * feature: remove old block lobrary lists
    
    * feature: add sections
    
    * feature: change to JS instead of json for section
    
    * feature: removing plugin config since it is not a plugin
    
    * feature: trying another way
    
    * feature: adding full plugins now
    
    * feature: remove old sidekick files
    
    * add hlx.page to CSP
    
    * feature: change icon fetch URL for testing
    
    * feature: adding style for library plugin to csp
    
    * feature: commenting out the css
    
    * feature: add height
    
    * feature: new sidekick version
    
    * fix: moved createOptimizedPicture
    
    * fix: block library icons host URL updated for merging
    
    * update test
    
    * fix: test
    
    * fix: linting
    
    * resolve conflicts
    
    * fix more conflicts
    
    * plugin sections
    
    * linting of js
    
    * fix: library location and compound sections will copy
    
    * fix: prevent template and block library pages from being published
    
    * fix: remove comment
    
    * small update to image path
    
    * removed comment
    
    * removing unnecessary section code
    
    * remove block-utils.test.js
    
    ---------
    
    Co-authored-by: Putra Bonaccorsi <putra.roeung@gmail.com>

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -422,45 +422,7 @@ export async function loadBlocks(main) {
   }
 }
 
-/**
- * Returns a picture element with webp and fallbacks
- * @param {string} src The image URL
- * @param {boolean} eager load image eager
- * @param {Array} breakpoints breakpoints and corresponding params (eg. width)
- */
-export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }]) {
-  const url = new URL(src, window.location.href);
-  const picture = document.createElement('picture');
-  const { pathname } = url;
-  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
-
-  // webp
-  breakpoints.forEach((br) => {
-    const source = document.createElement('source');
-    if (br.media) source.setAttribute('media', br.media);
-    source.setAttribute('type', 'image/webp');
-    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
-    picture.appendChild(source);
-  });
-
-  // fallback
-  breakpoints.forEach((br, i) => {
-    if (i < breakpoints.length - 1) {
-      const source = document.createElement('source');
-      if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-      picture.appendChild(source);
-    } else {
-      const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
-      img.setAttribute('alt', alt);
-      picture.appendChild(img);
-      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-    }
-  });
-
-  return picture;
-}
+// createOptimizedPicture moved to scripts.js
 
 /**
  * Normalizes all headings within a container element.
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,6 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
-  createOptimizedPicture,
   readBlockConfig,
 } from './lib-franklin.js';
 
@@ -26,6 +25,77 @@ window.hlx.RUM_GENERATION = 'merative'; // add your RUM generation information h
  */
 export function locationCheck(keyword) {
   return window.location.pathname.includes(keyword);
+}
+
+/**
+ * Added 2023-06-26 per new sidekick instructions
+ * Returns the true origin of the current page in the browser.
+ * If the page is running in an iframe with srcdoc, the ancestor origin is returned.
+ * @returns {String} The true origin
+ */
+
+export function getOrigin() {
+  const { location } = window;
+  return location.href === 'about:srcdoc' ? window.parent.location.origin : location.origin;
+}
+
+/**
+ * Returns the true of the current page in the browser.mac
+ * If the page is running in a iframe with srcdoc,
+ * the ancestor origin + the path query param is returned.
+ * @returns {String} The href of the current page or the href of the block running in the library
+ */
+
+export function getHref() {
+  if (window.location.href !== 'about:srcdoc') return window.location.href;
+
+  const { location: parentLocation } = window.parent;
+  const urlParams = new URLSearchParams(parentLocation.search);
+  return `${parentLocation.origin}${urlParams.get('path')}`;
+}
+
+/** moved from lib-franklin.js per https://github.com/adobe/franklin-sidekick-library#considerations-when-building-blocks-for-the-library
+ * Returns a picture element with webp and fallbacks
+ * @param {string} src The image URL
+ * @param {boolean} eager load image eager
+ * @param {Array} breakpoints breakpoints and corresponding params (eg. width)
+ */
+
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{
+  media: '(min-width: 600px)',
+  width: '2000',
+}, { width: '750' }]) {
+  const url = new URL(src, getHref());
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+    }
+  });
+
+  return picture;
 }
 
 /**
